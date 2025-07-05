83478da6a0fcbaee5d1696228d266de8da729d7d
Mahmud-Araf <almahmudaraf@gmail.com>
2025-07-05 15:40:16 +0600

    added static file hosting

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from models import Base
 from db import engine
+import os
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -14,8 +16,12 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-Base.metadata.drop_all(bind=engine)  
+# Base.metadata.drop_all(bind=engine)  
 Base.metadata.create_all(bind=engine)  
+
+
+os.makedirs("media", exist_ok=True)
+app.mount("/api/v1/media", StaticFiles(directory="media"), name="media")
 
 @app.get("/")
 def read_root():
