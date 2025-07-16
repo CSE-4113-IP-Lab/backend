1c74a47db051f4a1966337aa7431305798fd8a9e
Mahmud Araf <96352037+Mahmud-Araf@users.noreply.github.com>
2025-07-17 01:23:17 +0600

    done for today (#21)
    * done for today
    
    * added testing pass
    
    * added secrets
    
    * added secrets
    
    * added secrets

--- agent/llm.py
+++ agent/llm.py
@@ -1,15 +1,19 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+
 import os
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 # Load environment variables
 load_dotenv()
 
+
 gemini = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=0,
-    timeout=60,  
-    max_retries=2,
-    transport="rest",
-    google_api_key=os.getenv("GOOGLE_API_KEY")
-)
+        model="gemini-2.5-flash",
+        temperature=0,
+        timeout=60,
+        max_retries=2,
+        transport="rest",
+        google_api_key=os.getenv("GOOGLE_API_KEY")
+    )
+
